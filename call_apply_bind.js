1b0214b24976e4e630ee783b6e98a956d8969d15
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-04-18 09:44:22 -0700

    add comments

--- call_apply_bind.js
+++ call_apply_bind.js
@@ -10,11 +10,13 @@ var addToThis = function (a, b) {
 
 // Call
 var ans = addToThis.call(obj, 1, 3);  //function_name.call(obj, function_args)
+console.log("call function on obj, 2 + 1 + 3");
 console.log(ans);
 
 // Apply
 var arr = [1, 2, 3];
 var ans = addToThis.apply(obj, arr);
+console.log("call function on obj, 2 + 1 + 2");
 console.log(ans);
 
 var ans = addToThis.apply(obj2, arr);
