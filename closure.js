8c5aa3b2b61d8e0f49c536bed8837a369ee935eb
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-06-07 12:37:38 -0700

    update

--- closure.js
+++ closure.js
@@ -1,3 +1,4 @@
+// Currying
 var addTo = function (passed) {
     var add = function (inner) {
         return passed + inner;
@@ -7,6 +8,6 @@ var addTo = function (passed) {
 
 console.log(addTo(3));   // function : add
 
-var addThree = new addTo(3);
+var addThree = addTo(3);
 
 console.log(addThree(1));   // 4
