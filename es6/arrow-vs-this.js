a8431f669537d75a20cf6640d2b6c8f593586721
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-07-20 11:55:39 -0700

    refactor

--- es6/arrow-vs-this.js
+++ es6/arrow-vs-this.js
@@ -12,6 +12,9 @@ user.sayHi();
 
 // Use arrow function fix the issue.
 // Because arrow function do not bind to "this" keyword, it use parent's "this" keyword
+// arrow functions are bound to their lexical scope. 
+// This means that inside an arrow function the value of "this" is
+// the same as in the parent block:
 let user2 = {
     name: "Ben",
     sayHi: function () {
