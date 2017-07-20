673b9201c73e00951a5329727cba790a3a386d34
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-07-19 23:31:10 -0700

    arrow function

--- es6/arrow-vs-this.js
+++ es6/arrow-vs-this.js
@@ -1,16 +1,23 @@
 let user = {
     name: "Ben",
     sayHi: function () {
-        console.log(this.name);
+        //let self = this; // fix
+        setTimeout(function () {
+            console.log(this.name)
+        }, 1000);
     }
 }
 
 user.sayHi();
 
+// Use arrow function fix the issue.
+// Because arrow function do not bind to "this" keyword, it use parent's "this" keyword
 let user2 = {
     name: "Ben",
-    sayHi: () => {
-        console.log(this.name);
+    sayHi: function () {
+         setTimeout(()=> {
+            console.log(this.name)
+        }, 1000);
     }
 }
 
