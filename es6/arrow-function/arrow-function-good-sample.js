d7ab0ecfdb1ab68c598c366ebda1176570e2640b
Benjamin Huang <bhuang@rms.com>
2019-04-07 12:58:17 -0700

    update

---
+++ es6/arrow-function/arrow-function-good-sample.js
@@ -0,0 +1,23 @@
+const event = {
+  name: 'Birthday Party',
+  guestList: ['Ben', 'Lily','Mike'],
+  printGuestList(){
+    const that = this;
+    this.guestList.forEach(function (guest){
+      console.log(guest + ' is attending '+ that.name);
+    });
+  }
+}
+
+const event_better = {
+  name: 'Birthday Party',
+  guestList: ['Ben', 'Lily','Mike'],
+  printGuestList(){
+    const that = this;
+    this.guestList.forEach((guest)=>{
+      console.log(guest + ' is attending '+ this.name);
+    });
+  }
+}
+
+event.printGuestList();
