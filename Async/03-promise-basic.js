ba94fa948255f5aafceddda09047ecd4325f935f
Benjamin Huang <bhuang@rms.com>
2020-01-03 09:44:11 -0800

    update

--- Async/03-promise-basic.js
+++ Async/03-promise-basic.js
@@ -12,41 +12,44 @@ createAudioFileAsync(audioSettings, successCallback, failureCallback);
 // Using permison
 createAudioFileAsync(audioSettings).then(successCallback, failureCallback);
 
-const promise = createAudioFileAsync(audioSettings); 
+const promise = createAudioFileAsync(audioSettings);
 promise.then(successCallback, failureCallback);
 
+/* 
+ File process using promise
 
-// File process using promise
+*/
 const fs = require('fs');
+// creat promise
 const text = new Promise(function (resolve, reject) {
-    // Normal fs.readFile call, but inside Promise constructor . . . 
-    fs.readFile('text.txt', function (err, text) {
-      // . . . Call reject if there's an error . . . 
-      if (err) 
-        reject(err);
-      // . . . And call resolve otherwise.
-      else
-    // We need toString() because fs.readFile returns a buffer.
-        resolve(text.toString());
-    })
-  });
+  // Normal fs.readFile call, but inside Promise constructor . . . 
+  fs.readFile('text.txt', function (err, text) {
+    // . . . Call reject if there's an error . . . 
+    if (err)
+      reject(err);
+    // . . . And call resolve otherwise.
+    else
+      // We need toString() because fs.readFile returns a buffer.
+      resolve(text.toString());
+  })
+});
 
-  text.then(function(result) {
-    console.log("Promise worked");
-  }, function(err) {
-    console.log("Something broke");
-  });
+text.then(function (result) {
+  console.log("Promise worked");
+}, function (err) {
+  console.log("Something broke");
+});
 
 // promise using XMLHttpRequest load image.
 const loadImage = url => {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
 
     //Open a new XHR
     var request = new XMLHttpRequest();
     request.open('GET', url);
 
     // When the request loads, check whether it was successful
-    request.onload = function() {
+    request.onload = function () {
       if (request.status === 200) {
         // If successful, resolve the promise
         resolve(request.response);
@@ -61,13 +64,13 @@ const loadImage = url => {
 };
 
 const embedImage = url => {
-  loadImage(url).then(function(result) {
-      const img = new Image();
-      var imageURL = window.URL.createObjectURL(result);
-      img.src = imageURL;
-      document.querySelector('body').appendChild(img);
-    },
-    function(err) {
+  loadImage(url).then(function (result) {
+    const img = new Image();
+    var imageURL = window.URL.createObjectURL(result);
+    img.src = imageURL;
+    document.querySelector('body').appendChild(img);
+  },
+    function (err) {
       console.log(err);
     });
 }
