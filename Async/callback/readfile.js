eb612b112fc0455494104d9c781c734bbeced503
Benjamin Huang <bhuang@rms.com>
2020-04-06 11:33:52 -0700

    ove async to a mew project

--- Async/callback/readfile.js
+++
@@ -1,9 +0,0 @@
-var fs = require("fs");
-fs.readFile(__dirname+"/test.txt", "utf8", function(err, content){
-    if (error){
-        console.error(error);
-        return;
-    }
-
-    console.log(content);
-});
