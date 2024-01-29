8b06cbdabe7365663ae02ae125316adb65789c5b
BenjaminHuang <benjaminhuanghuang@gmail.com>
2024-01-29 11:44:53 -0800

    update

---
+++ _Projects/vanillaJS-game/README.md
@@ -0,0 +1,7 @@
+# JavaScript GameDev Tutorial – Code an Animated Physics Game [Full Course]
+
+2023.2
+
+https://www.youtube.com/watch?v=U34l-Xz5ynU
+
+HTML + CSS + vanilla JS
---
+++ _Projects/vanillaJS-game/bullseye/script.js
@@ -0,0 +1,107 @@
+window.addEventListener("load", function () {
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+
+  canvas.width = 1280;
+  canvas.height = 720;
+
+  ctx.fillStyle = "white";
+  ctx.lineWidth = 3;
+  ctx.storeStyle = "white";
+
+  class Player {
+    constructor(game) {
+      this.game = game;
+      this.collisionX = this.game.width * 0.5;
+      this.collisionY = this.game.height * 0.5;
+      this.collisionRadius = 30;
+      this.speedX = 0;
+      this.speedY = 0;
+      this.dx = 0;
+      this.dy = 0;
+      this.speedModifier = 20;
+    }
+
+    draw(context) {
+      context.beginPath();
+      context.arc(
+        this.collisionX,
+        this.collisionY,
+        this.collisionRadius,
+        0,
+        Math.PI * 2
+      );
+      context.fill();
+    }
+
+    update() {
+      this.dx = this.game.mouse.x - this.collisionX;
+      this.dy = this.game.mouse.y - this.collisionY;
+
+      const distance = Math.hypot(this.dy, this.dx);
+    if(distance > this.speedModifier){
+      this.speedX = this.dx / distance || 0;
+      this.speedY = this.dy / distance || 0;
+    }else {
+        this.speedX=0;
+        this.speedY=0;
+    }
+      this.collisionX += this.speedX * this.speedModifier;
+      this.collisionY += this.speedY * this.speedModifier;
+    }
+  }
+
+  class Obstacle {
+
+  }
+
+  
+  class Game {
+    constructor() {
+      this.canvas = canvas;
+      this.width = canvas.width;
+      this.height = canvas.height;
+
+      this.player = new Player(this);
+      this.mouse = {
+        x: this.width * 0.5,
+        y: this.height * 0.5,
+        pressed: false,
+      };
+      // event listeners
+      canvas.addEventListener("mousedown", (e) => {
+        this.mouse.x = e.offsetX;
+        this.mouse.y = e.offsetY;
+        this.mouse.pressed = true;
+      });
+
+      canvas.addEventListener("mouseup", (e) => {
+        this.mouse.x = e.offsetX;
+        this.mouse.y = e.offsetY;
+        this.mouse.pressed = false;
+      });
+
+      canvas.addEventListener("mousemove", (e) => {
+        if (this.mouse.pressed) {
+          this.mouse.x = e.offsetX;
+          this.mouse.y = e.offsetY;
+        }
+      });
+    }
+
+    render(context) {
+      this.player.draw(context);
+      this.player.update();
+    }
+  }
+
+  const game = new Game(canvas);
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.render(ctx);
+    window.requestAnimationFrame(animate);
+  }
+
+  animate();
+});
